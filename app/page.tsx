57ac278443c1623d3d48913ac3ea49f3bb59bff3
jsakmar <jozef.sakmar@gmail.com>
2026-04-24 23:41:33 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-return (
-  <main>
-    <TempChart data={data} />
-  </main>
-)
+import TempChart from "../components/TempChart"
+
+export default async function Page() {
+  const data = [
+    { day: "Fri", max: 13, min: 4 },
+    { day: "Sat", max: 17, min: 2 },
+  ]
+
+  return (
+    <main>
+      <TempChart data={data} />
+    </main>
+  )
+}
