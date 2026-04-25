1b57908021fc6639e681c8002d561c27b2887b79
jsakmar <jozef.sakmar@gmail.com>
2026-04-25 21:13:06 +0200

    Create wind.ts

---
+++ lib/wind.ts
@@ -0,0 +1,14 @@
+export function getWindRotation(dir: string) {
+  const map: Record<string, number> = {
+    N: 0,
+    NE: 45,
+    E: 90,
+    SE: 135,
+    S: 180,
+    SW: 225,
+    W: 270,
+    NW: 315,
+  }
+
+  return map[dir] ?? 0
+}
